89b32d1321e4e55a755ad9ee4b094f228e71c563
Riya Manchanda <riyamanchanda10@gmail.com>
2022-03-15 00:59:16 +0530

    converted java classes to kotlin

---
+++ app/src/main/java/com/serviceapps/shopping/ui/adapters/SellerOrderPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.serviceapps.shopping.ui.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import java.util.ArrayList
+
+class SellerOrderPagerAdapter(fm: FragmentManager?) : FragmentPagerAdapter(
+    fm!!
+) {
+    private val fragmentList: MutableList<Fragment> = ArrayList()
+    private val titleList: MutableList<String> = ArrayList()
+    override fun getItem(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getCount(): Int {
+        return fragmentList.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return titleList[position]
+    }
+
+    fun addFragment(fragment: Fragment, title: String) {
+        fragmentList.add(fragment)
+        titleList.add(title)
+    }
+}
---
+++ app/src/main/java/com/serviceapps/shopping/ui/fragments/SellerOrdersBaseFragment.kt
@@ -0,0 +1,49 @@
+package com.serviceapps.shopping.ui.fragments
+
+import com.serviceapps.shopping.ui.fragments.BaseFragment
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.os.Bundle
+import android.view.View
+import com.serviceapps.shopping.R
+import com.google.android.material.tabs.TabLayout.OnTabSelectedListener
+import com.serviceapps.shopping.ui.adapters.SellerOrderPagerAdapter
+import kotlinx.android.synthetic.main.seller_orders_base_layout.*
+
+class SellerOrdersBaseFragment : BaseFragment() {
+    var myFragment: View? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        myFragment = inflater.inflate(R.layout.seller_orders_base_layout, container, false)
+        return myFragment
+    }
+
+    //Call onActivity Create method
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        setUpViewPager(viewPager)
+        tabLayout!!.setupWithViewPager(viewPager)
+        tabLayout!!.addOnTabSelectedListener(object : OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {}
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+        })
+    }
+
+    private fun setUpViewPager(viewPager: ViewPager?) {
+        val adapter = SellerOrderPagerAdapter(childFragmentManager)
+        adapter.addFragment(OrdersFragment(), "Customer Orders")
+        adapter.addFragment(SellerOrdersFragment(), "Your Orders")
+        viewPager!!.adapter = adapter
+    }
+
+    companion object {
+        val instance: SellerOrdersBaseFragment
+            get() = SellerOrdersBaseFragment()
+    }
+}
